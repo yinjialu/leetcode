0ddf4bfcb82fd712e2b1e68b829e081c05bfe7e5
yinjialu <yinjialu1213@gmail.com>
2020-12-10 08:57:04 +0800

    v1:  [10] 正则表达式匹配

--- .leetcode/10.正则表达式匹配.js
+++ .leetcode/10.正则表达式匹配.js
@@ -16,18 +16,20 @@
 var isMatch = function (s, p) {
   const sL = s.length
   const pL = p.length
+  const map = {}
   const check = (sC, pC) => {
+    if (map[`${sC}:${pC}`] !== undefined) return map[`${sC}:${pC}`]
     if (sC > sL) return false
     if (sC === sL && pC === pL) return true
     const sV = s.slice(sC, sC + 1)
     const pV = p.slice(pC, pC + 1)
     const pNext = p.slice(pC + 1, pC + 2)
     if (sV === pV || pV === '.') {
-      if (pNext === '*') return check(sC + 1, pC) || check(sC, pC + 2)
-      return check(sC + 1, pC + 1)
+      map[`${sC}:${pC}`] = pNext === '*' ? check(sC + 1, pC) || check(sC, pC + 2) : check(sC + 1, pC + 1)
+    } else {
+      map[`${sC}:${pC}`] = pNext === '*' ? check(sC, pC + 2) : false
     }
-    if (pNext === '*') return check(sC, pC + 2)
-    return false
+    return map[`${sC}:${pC}`]
   }
   return check(0, 0)
 }
@@ -38,3 +40,8 @@ var isMatch = function (s, p) {
 
 // bbbba
 // .*a*a
+
+// Accepted
+// 447/447 cases passed (88 ms)
+// Your runtime beats 98.94 % of javascript submissions
+// Your memory usage beats 26.88 % of javascript submissions (41.6 MB)
