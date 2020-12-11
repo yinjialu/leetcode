ef1b4bba5a4c37e93d9f7b1862732418a035a85b
yinjialu <yinjialu1213@gmail.com>
2020-12-12 07:46:07 +0800

    v1: [12] 整数转罗马数字

--- .leetcode/12.整数转罗马数字.js
+++ .leetcode/12.整数转罗马数字.js
@@ -10,48 +10,24 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    const I = 1
-    const V = 5
-    const X = 10
-    const L = 50
-    const C = 100
-    const D = 500
-    const M = 1000
+    const map = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M',
+    }
     let v = num
     let unit = 1000
     let s = ''
     const format = (unit, val) => {
       if (val <= 0) return ''
-      if (unit === 1000) {
-        return new Array(val).fill('M').join('')
-      }
-      if (unit === 100) {
-        if (val > 5) {
-          if (val === 9) return 'CM'
-          return `D${new Array(val - 5).fill('C').join('')}`
-        }
-        if (val === 5) return 'D'
-        if (val === 4) return 'CD'
-        return new Array(val).fill('C').join('')
-      }
-      if (unit === 10) {
-        if (val > 5) {
-          if (val === 9) return 'XC'
-          return `L${new Array(val - 5).fill('X').join('')}`
-        }
-        if (val === 5) return 'L'
-        if (val === 4) return 'XL'
-        return new Array(val).fill('X').join('')
-      }
-      if (unit === 1) {
-        if (val > 5) {
-          if (val === 9) return 'IX'
-          return `V${new Array(val - 5).fill('I').join('')}`
-        }
-        if (val === 5) return 'V'
-        if (val === 4) return 'IV'
-        return new Array(val).fill('I').join('')
-      }
+      if (val === 5) return map[unit * val];
+      if (val === 9) return `${map[unit]}${map[unit * 10]}`;
+      if (val === 4) return `${map[unit]}${map[unit * 5]}`;
+      return val > 5 ? map[unit * 5] + new Array(val - 5).fill(map[unit]).join('') : new Array(val).fill(map[unit]).join('');
     }
     while (v >= 1) {
       const n = Math.floor(v / unit)
@@ -68,3 +44,8 @@ var intToRoman = function(num) {
 // Your runtime beats 44.49 % of javascript submissions
 // Your memory usage beats 22.54 % of javascript submissions (44.5 MB)
 
+// v1
+// Accepted
+// 3999/3999 cases passed (172 ms)
+// Your runtime beats 44.49 % of javascript submissions
+// Your memory usage beats 30.75 % of javascript submissions (44.2 MB)
