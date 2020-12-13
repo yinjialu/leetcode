9436c327b499abb6344bf28f30ec508a89bba921
yinjialu <yinjialu1213@gmail.com>
2020-12-13 14:58:15 +0800

    new: [17] 电话号码的字母组合

---
+++ .leetcode/17.电话号码的字母组合.js
@@ -0,0 +1,45 @@
+/**
+ * /*
+ *
+ * [17] 电话号码的字母组合
+ *
+ * @format
+ * @lc app=leetcode.cn id=17 lang=javascript
+ */
+
+// @lc code=start
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+  const map = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  }
+  const len = digits.length
+  const fun = (arr1, i) => {
+    if (i === len) return arr1
+    const val = digits.slice(i, i + 1)
+    if (!val) return arr1
+    let arr = []
+    const arr2 = map[val]
+    arr2.forEach((a2) => {
+      arr = arr.concat(arr1.map((a1) => `${a1}${a2}`))
+    })
+    if (i < len) return fun(arr, i + 1)
+  }
+  return fun(map[digits.slice(0, 1)] || [], 1)
+}
+// @lc code=end
+
+// Accepted
+// 25/25 cases passed (92 ms)
+// Your runtime beats 19.91 % of javascript submissions
+// Your memory usage beats 42.56 % of javascript submissions (37.7 MB)
