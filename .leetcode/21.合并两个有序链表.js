8cd300e6b3a6ef4beb3938b35616e50728f7682e
yinjialu <yinjialu1213@gmail.com>
2020-12-15 07:59:50 +0800

    new: [21] 合并两个有序链表

---
+++ .leetcode/21.合并两个有序链表.js
@@ -0,0 +1,61 @@
+/**
+ * /*
+ *
+ * [21] 合并两个有序链表
+ *
+ * @format
+ * @lc app=leetcode.cn id=21 lang=javascript
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let n1 = l1
+  let n2 = l2
+  let head
+  let last
+  if (!n1) {
+    if (!n2) return null
+    return n2
+  } else if (!n2) return n1
+  do {
+    if (n1.val < n2.val) {
+      head = head || n1
+      if (last) {
+        last.next = n1
+        last = n1
+      } else {
+        last = head
+      }
+      n1 = n1.next
+    } else {
+      head = head || n2
+      if (last) {
+        last.next = n2
+        last = n2
+      } else {
+        last = head
+      }
+      n2 = n2.next
+    }
+  } while (n1 && n2)
+  last.next = n1 ? n1 : n2
+  return head
+}
+// @lc code=end
+
+// Accepted
+// 208/208 cases passed (104 ms)
+// Your runtime beats 24.46 % of javascript submissions
+// Your memory usage beats 41.06 % of javascript submissions (39.4 MB)
