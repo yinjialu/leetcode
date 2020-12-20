87ed05036af77569b90877965dfe3951086740a4
yinjialu <yinjialu1213@gmail.com>
2020-12-20 09:23:46 +0800

    new: [28] 实现 strStr()

---
+++ .leetcode/28.实现-str-str.js
@@ -0,0 +1,24 @@
+/**
+ * /*
+ *
+ * [28] 实现 strStr()
+ *
+ * @format
+ * @lc app=leetcode.cn id=28 lang=javascript
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle)
+}
+// @lc code=end
+
+// Accepted
+// 78/78 cases passed (64 ms)
+// Your runtime beats 99.89 % of javascript submissions
+// Your memory usage beats 91.21 % of javascript submissions (37.1 MB)
