0540b4e586968b2f949929070e113b32b4ed9a7f
yinjialu <yinjialu1213@gmail.com>
2020-12-26 10:41:43 +0800

    v1: [32] 最长有效括号

--- .leetcode/32.最长有效括号.js
+++ .leetcode/32.最长有效括号.js
@@ -13,21 +13,40 @@
  * @return {number}
  */
 var longestValidParentheses = function (s) {
-  let maxlen = 0
-  const stock = [-1]
+  let leftCount = 0;
+  let rightCount = 0;
+  let maxLen = 0;
   for (let i = 0; i < s.length; i++) {
-    const v = s.charAt(i)
-    if (v === '(') {
-      stock.push(i)
-      continue
-    }
-    stock.pop()
-    if (stock.length === 0) {
-      stock.push(i)
-    }
-    maxlen = Math.max(maxlen, i - stock[stock.length - 1])
+      const v = s.charAt(i);
+      if (v === '(') {
+          leftCount++;
+      } else {
+          rightCount++;
+      }
+      if (leftCount === rightCount) {
+          maxLen = Math.max(maxLen, leftCount + rightCount);
+      } else if (rightCount > leftCount) {
+          leftCount = 0;
+          rightCount = 0;
+      }
   }
-  return maxlen
+  leftCount = 0;
+  rightCount = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+      const v = s.charAt(i);
+      if (v === ')') {
+          rightCount++;
+      } else {
+          leftCount++;
+      }
+      if (rightCount === leftCount) {
+          maxLen = Math.max(maxLen, rightCount + leftCount);
+      } else if (leftCount > rightCount) {
+          leftCount = 0;
+          rightCount = 0;
+      }
+  }
+  return maxLen
 }
 // @lc code=end
 
@@ -37,7 +56,41 @@ var longestValidParentheses = function (s) {
 // (()(((()
 // (()(((())))())
 
+// var longestValidParentheses = function (s) {
+//     let maxlen = 0
+//     const stock = [-1]
+//     for (let i = 0; i < s.length; i++) {
+//         const v = s.charAt(i)
+//         if (v === '(') {
+//         stock.push(i)
+//         continue
+//         }
+//         stock.pop()
+//         if (stock.length === 0) {
+//         stock.push(i)
+//         }
+//         maxlen = Math.max(maxlen, i - stock[stock.length - 1])
+//     }
+//     return maxlen
+// }
+
 // Accepted
 // 231/231 cases passed (100 ms)
 // Your runtime beats 40.72 % of javascript submissions
 // Your memory usage beats 82.63 % of javascript submissions (39.6 MB)
+
+/**
+ * (()
+ * 方案2: 
+ * 从左到右遍历一遍
+ * 统计 '(', ')' 出现的次数，如果 '(' 和 ')' 的次数一致，刷新最大长度，如果 ')' 出现的次数比 '(' 大，重置累计的长度
+ *
+ * 从右到左遍历一遍
+ * 统计 ')', '(' 出现的次数，如果 ')' 和 '(' 的次数一致，刷新最大长度，如果 '(' 出现的次数比 ')' 大，重置累计的长度
+ */
+
+// v1
+// Accepted
+// 231/231 cases passed (88 ms)
+// Your runtime beats 80.54 % of javascript submissions
+// Your memory usage beats 88.67 % of javascript submissions (39.4 MB)
