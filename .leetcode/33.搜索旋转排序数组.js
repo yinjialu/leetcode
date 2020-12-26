4ffc4c89dee1ca953c6caa0b3567fb35dfb77ed0
yinjialu <yinjialu1213@gmail.com>
2020-12-26 13:05:08 +0800

    feat: [33] 搜索旋转排序数组

--- .leetcode/33.搜索旋转排序数组.js
+++ .leetcode/33.搜索旋转排序数组.js
@@ -14,14 +14,29 @@
  * @return {number}
  */
 var search = function (nums, target) {
-  let result = -1
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      result = i
-      break
+    // 纯递增数组使用
+    const fun1 = (left, right) => {
+        if (nums[left] > target) return;
+        if (nums[right] < target) return;
+        const middle = Math.floor((left + right) / 2);
+        if (nums[middle] === target) return middle
+        if (nums[left] === target) return left;
+        if (nums[right] === target) return right;
+        if (left === middle) return
+        if (nums[middle] > target) return fun1(left, middle);
+        return fun1(middle, right);
     }
-  }
-  return result
+    const fun2 = (left, right) => {
+        const middle = Math.floor((left + right) / 2);
+        if (nums[middle] === target) return middle
+        if (nums[left] === target) return left;
+        if (nums[right] === target) return right;
+        if (left === middle) return
+        if (nums[left] < nums[middle]) return fun1(left, middle) || fun2(middle, right);
+        return fun1(middle, right) || fun2(left, middle);
+    }
+    const result = fun2(0, nums.length - 1);
+    return result === undefined ? -1 : result
 }
 // @lc code=end
 
@@ -29,3 +44,9 @@ var search = function (nums, target) {
 // 195/195 cases passed (136 ms)
 // Your runtime beats 7.48 % of javascript submissions
 // Your memory usage beats 53.68 % of javascript submissions (38.9 MB)
+
+//v1
+// Accepted
+// 195/195 cases passed (84 ms)
+// Your runtime beats 67.96 % of javascript submissions
+// Your memory usage beats 75.17 % of javascript submissions (38.7 MB)
