30d030ba4cd06cc02286f0026238cf4a0af0c97a
yinjialu <yinjialu1213@gmail.com>
2020-12-27 20:42:26 +0800

    v1: [35] 搜索插入位置

--- .leetcode/35.搜索插入位置.js
+++ .leetcode/35.搜索插入位置.js
@@ -14,10 +14,40 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] >= target) return i
+  let left = 0
+  let right = nums.length - 1
+  let result = nums.length
+  while (left <= right) {
+    if (nums[right] < target) {
+      result = right + 1
+      break
+    }
+    if (nums[right] === target) {
+      result = right
+      break
+    }
+    if (nums[left] >= target) {
+      result = left
+      break
+    }
+    // nums[left] < target < nums[right]
+    const middle = (left + right) >> 1
+    if (nums[middle] === target) {
+      result = middle
+      break
+    }
+    if (target < nums[middle]) {
+      // target 在 lert - middle 之间
+      right = middle - 1
+      result = middle
+    } else {
+      // nums[middle] < target
+      // target 在 middle - right 之间
+      left = middle + 1
+      result = right
+    }
   }
-  return nums.length
+  return result
 }
 // @lc code=end
 
@@ -25,3 +55,9 @@ var searchInsert = function (nums, target) {
 // 62/62 cases passed (88 ms)
 // Your runtime beats 40.55 % of javascript submissions
 // Your memory usage beats 85.5 % of javascript submissions (38.3 MB)
+
+// v1
+// Accepted
+// 62/62 cases passed (88 ms)
+// Your runtime beats 40.55 % of javascript submissions
+// Your memory usage beats 85.59 % of javascript submissions (38.3 MB)
