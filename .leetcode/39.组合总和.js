c432c462ce0c3360088d74b868e6fb2bd5ed7735
yinjialu <yinjialu1213@gmail.com>
2021-01-02 12:44:30 +0800

    v1: [39] 组合总和

--- .leetcode/39.组合总和.js
+++ .leetcode/39.组合总和.js
@@ -13,24 +13,27 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function (candidates, target, start = 0) {
+var combinationSum = function (candidates, target) {
   const min = Math.min(...candidates)
-  let result = []
-  for (let i = start; i < candidates.length; i++) {
-    const v = candidates[i]
-    if (v === target) {
-      result.push([v])
-    } else if (target - v >= min) {
-      const subArr = combinationSum(candidates, target - v, i)
-      if (subArr.length > 0) {
-        subArr.forEach((s) => {
-          s.unshift(v)
-        })
+  const fun = (target, start) => {
+    let result = []
+    for (let i = start; i < candidates.length; i++) {
+      const v = candidates[i]
+      if (v === target) {
+        result.push([v])
+      } else if (target - v >= min) {
+        const subArr = fun(target - v, i)
+        if (subArr.length > 0) {
+          subArr.forEach((s) => {
+            s.unshift(v)
+          })
+        }
+        result = result.concat(subArr)
       }
-      result = result.concat(subArr)
     }
+    return result
   }
-  return result
+  return fun(target, 0)
 }
 // @lc code=end
 
@@ -38,3 +41,9 @@ var combinationSum = function (candidates, target, start = 0) {
 // 170/170 cases passed (96 ms)
 // Your runtime beats 85.66 % of javascript submissions
 // Your memory usage beats 63.86 % of javascript submissions (40.2 MB)
+
+// v1
+// Accepted
+// 170/170 cases passed (88 ms)
+// Your runtime beats 96.7 % of javascript submissions
+// Your memory usage beats 84.35 % of javascript submissions (40 MB)
