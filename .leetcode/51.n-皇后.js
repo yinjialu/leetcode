65745318483209d6cad3e699e60e72ae03473fdb
yinjialu <yinjialu1213@gmail.com>
2021-01-17 11:12:39 +0800

    v1 [51] N 皇后

--- .leetcode/51.n-皇后.js
+++ .leetcode/51.n-皇后.js
@@ -14,110 +14,39 @@
  */
 var solveNQueens = function (n) {
     const result = [];
-    const arr = new Array(n).fill(new Array(n));
-    const map = {};
-    const getCloseDots = (i, j) => {
-        if (map[`${i}${j}`]) return map[`${i}${j}`];
-        const getRightBottom = (i, j) => {
-            const arr = [];
-            i++;
-            j++;
-            while (i < n && j < n) {
-                arr.push([i, j]);
-                i++;
-                j++;
-            }
-            return arr;
-        };
-        const getRightTop = (i, j) => {
-            const arr = [];
-            i--;
-            j++;
-            while (i >= 0 && j < n) {
-                arr.push([i, j]);
-                i--;
-                j++;
-            }
-            return arr;
-        };
-        const getLeftBottom = (i, j) => {
-            const arr = [];
-            i++;
-            j--;
-            while (i < n && j >= 0) {
-                arr.push([i, j]);
-                i++;
-                j--;
-            }
-            return arr;
-        };
-        const getLeftTop = (i, j) => {
-            const arr = [];
-            i--;
-            j--;
-            while (i >= 0 && j >= 0) {
-                arr.push([i, j]);
-                i--;
-                j--;
-            }
-            return arr;
-        };
-        let arr = [];
-        for (let a = 0; a < n; a++) {
-            if (a === j) continue;
-            arr.push([i, a]);
+    const checkUseFulPoint = (res, line) => {
+        const arr1 = [];
+        for (let p = 0; p < n; p++) {
+            if (res.some(([i, j]) => j === p)) continue;
+            if (res.some(([i, j]) => Math.abs(line - i) === Math.abs(p - j)))
+                continue;
+            arr1.push([line, p]);
         }
-        for (let b = 0; b < n; b++) {
-            if (b === i) continue;
-            arr.push([b, j]);
-        }
-        arr = arr.concat(
-            getRightBottom(i, j),
-            getRightTop(i, j),
-            getLeftBottom(i, j),
-            getLeftTop(i, j)
-        );
-        map[`${i}${j}`] = arr;
-        return arr;
+        return arr1;
     };
-    const forword = (arr, i, j) => {
-        arr[i][j] = 'Q';
-        const closeDots = getCloseDots(i, j);
-        closeDots.forEach(([i, j]) => {
-            arr[i][j] = '.';
-        });
-    };
-    const back = (arr, i, j) => {
-        arr[i][j] = undefined;
-        const closeDots = getCloseDots(i, j);
-        closeDots.forEach(([i, j]) => {
-            a[i][j] = undefined;
-        });
-    };
-    const getEmptyPoint = (arr, line) => {
-        let r = [];
-        for (let j = 0; j < arr[line].length; j++) {
-            if (!arr[line][j]) {
-                r.push([line, j]);
-            }
-        }
-        return r;
-    };
-    const fun = (arr, line) => {
+    const fun2 = (res, line) => {
         if (line === n) {
-            result.push(arr.map((a) => a.join('')));
+            result.push(res);
             return;
         }
-        const emptyPoints = getEmptyPoint(arr, line);
-        if (emptyPoints.length === 0) return;
-        emptyPoints.forEach(([i, j]) => {
-            const copyArr = JSON.parse(JSON.stringify(arr));
-            forword(copyArr, i, j);
-            fun(copyArr, line + 1);
+        const useFullPoins = checkUseFulPoint(res, line);
+        if (useFullPoins.length === 0) return;
+        useFullPoins.forEach(([i, j]) => {
+            fun2(res.concat([[i, j]]), line + 1);
         });
     };
-    fun(arr, 0);
-    return result;
+    fun2([], 0);
+    const data = [];
+    result.forEach((r) => {
+        const arr = [];
+        r.forEach(([i, j]) => {
+            const arr1 = new Array(n).fill('.');
+            arr1[j] = 'Q';
+            arr.push(arr1.join(''));
+        });
+        data.push(arr);
+    });
+    return data;
 };
 // @lc code=end
 
@@ -125,3 +54,9 @@ var solveNQueens = function (n) {
 // 9/9 cases passed (192 ms)
 // Your runtime beats 5.04 % of javascript submissions
 // Your memory usage beats 5.05 % of javascript submissions (46.4 MB)
+
+// v1
+// Accepted
+// 9/9 cases passed (140 ms)
+// Your runtime beats 9.99 % of javascript submissions
+// Your memory usage beats 5.05 % of javascript submissions (45.6 MB)
