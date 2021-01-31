9941ad32df26ecb50e274774182162bb6235fec5
yinjialu <yinjialu1213@gmail.com>
2021-01-31 17:32:37 +0800

    v1: [62] 不同路径

--- .leetcode/62.不同路径.js
+++ .leetcode/62.不同路径.js
@@ -7,9 +7,7 @@
  * @lc app=leetcode.cn id=62 lang=javascript
  */
 
-// [0][0] => [n - 1][m - 1]
-// [0][0] => [2][1]
-// [0][0] => [2][6]
+// memo[row][col] 比 memo[`${row}_${col}`] 计算效率更高
 
 // @lc code=start
 /**
@@ -22,26 +20,31 @@ var uniquePaths = function (m, n) {
         if (row === m && col === n) return 1;
         if (row > m || col > n) return 0;
 
-        // if (memo[row][col] === -1) {
-        if (memo[`${row}_${col}`] === undefined) {
+        if (memo[row][col] === -1) {
+            // if (memo[`${row}_${col}`] === undefined) {
             const pathsRight = helper(m, n, row, col + 1, memo);
             const pathsDown = helper(m, n, row + 1, col, memo);
 
-            memo[`${row}_${col}`] = pathsRight + pathsDown;
+            memo[row][col] = pathsRight + pathsDown;
         }
 
-        return memo[`${row}_${col}`];
+        return memo[row][col];
     };
-    // const memo = new Array(m + 1).fill(0);
-    // for (let i = 0; i < memo.length; i++) {
-    //     memo[i] = new Array(n + 1).fill(-1);
-    // }
-    const memo = {};
+    const memo = new Array(m + 1).fill(0);
+    for (let i = 0; i < memo.length; i++) {
+        memo[i] = new Array(n + 1).fill(-1);
+    }
+    // const memo = {};
     return helper(m, n, 1, 1, memo);
-};
+};;
 // @lc code=end
 
 // Accepted
 // 62/62 cases passed (92 ms)
 // Your runtime beats 26.72 % of javascript submissions
 // Your memory usage beats 5.05 % of javascript submissions (39.6 MB)
+
+// Accepted
+// 62/62 cases passed (80 ms)
+// Your runtime beats 77.66 % of javascript submissions
+// Your memory usage beats 29.44 % of javascript submissions (38.1 MB)
