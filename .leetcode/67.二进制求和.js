ed5bbeb93f48bfe43b9eb2a6c3eb20badf1eee52
yinjialu <yinjialu1213@gmail.com>
2021-02-06 18:18:04 +0800

    feat: 支持扩展到其他进制

--- .leetcode/67.二进制求和.js
+++ .leetcode/67.二进制求和.js
@@ -15,19 +15,20 @@
  */
 var addBinary = function (a, b) {
     const v = [0, 1]; // 二进制，可以扩展其他进制
+    const vMap = [0, 1]; // 扩展到其他进展使用的对应的字母表示值
     const max = v[v.length - 1] + 1;
     let rest = 0;
     const maxLen = Math.max(a.length, b.length);
     let result = '';
     for (let i = 0; i < maxLen; i++) {
-        const va = +a.charAt(a.length - 1 - i) || 0;
-        const vb = +b.charAt(b.length - 1 - i) || 0;
-        const v = rest + va + vb;
-        rest = Math.floor(v / max);
-        result = (v % max) + result;
+        const va = v[+a.charAt(a.length - 1 - i) || 0];
+        const vb = v[+b.charAt(b.length - 1 - i) || 0];
+        const value = rest + va + vb;
+        rest = Math.floor(value / max);
+        result = vMap[value % max] + result;
     }
     if (rest) {
-        result = rest + result;
+        result = vMap[rest] + result;
     }
     return result;
 };
@@ -37,3 +38,9 @@ var addBinary = function (a, b) {
 // 294/294 cases passed (92 ms)
 // Your runtime beats 68.71 % of javascript submissions
 // Your memory usage beats 22.06 % of javascript submissions (40.2 MB)
+
+// v1
+// Accepted
+// 294/294 cases passed (84 ms)
+// Your runtime beats 92.05 % of javascript submissions
+// Your memory usage beats 11.68 % of javascript submissions (40.6 MB)
