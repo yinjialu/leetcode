80c4d2d3d5bdb43e868e5950cd6f0af46b1246b3
yinjialu <yinjialu1213@gmail.com>
2021-02-24 07:37:12 +0800

    new: [75] 颜色分类

---
+++ .leetcode/75.颜色分类.js
@@ -0,0 +1,43 @@
+/**
+ * /*
+ *
+ * [75] 颜色分类
+ *
+ * @format
+ * @lc app=leetcode.cn id=75 lang=javascript
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    const map = {
+        0: 0,
+        1: 0,
+        2: 0,
+    };
+    const getPosition = (v) => {
+        let count = 0;
+        while (v >= 0) {
+            count += map[v];
+            v--;
+        }
+        return count;
+    };
+    let i = 0;
+    while (i < nums.length) {
+        const v = nums[i];
+        const position = getPosition(v);
+        map[v]++;
+        nums.splice(position, 0, nums.splice(i, 1)[0]);
+        i++;
+    }
+};
+// @lc code=end
+
+// Accepted
+// 87/87 cases passed (96 ms)
+// Your runtime beats 19.37 % of javascript submissions
+// Your memory usage beats 23.73 % of javascript submissions (38.8 MB)
